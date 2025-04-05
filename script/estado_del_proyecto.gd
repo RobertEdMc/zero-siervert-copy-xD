8b8f07376a9e6962eea6a096350c595f5df7cf46
EdouardMc <104469772+Elver-Galarga-jpg@users.noreply.github.com>
2025-04-05 17:22:22 -0400

    Comienzo
    xd

---
+++ script/estado_del_proyecto.gd
@@ -0,0 +1,13 @@
+class_name ProyectState extends Node
+
+var activos :String 
+var trabajos :int = 1
+
+func _ready() -> void:
+	#setear la actividad
+	if trabajos > 0:
+		activos = "si"
+	else:
+		activos = "No"
+	print("Estamos activos:" + activos)
+	pass
